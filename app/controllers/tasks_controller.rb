cd61a90e191ad9ab4a3cdaa4afdbae68307f13c5
kurumi.m <kurumi0306maeda@gmail.com>
2023-08-19 08:58:13 +0000

    validation

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
   def index
     @tasks = Task.all
   end
   
   def show
-    @task = Task.find(params[:id])
   end
   
   def new
@@ -12,7 +13,6 @@ class TasksController < ApplicationController
   end
   
   def edit
-    @task = Task.find(params[:id])
   end
   
   def create
@@ -29,7 +29,6 @@ class TasksController < ApplicationController
   end
   
   def update
-    @task = Task.find(params[:id])
     
     if @task.update(task_params)
       flash[:success] = "Task は正常に更新されました"
@@ -42,7 +41,6 @@ class TasksController < ApplicationController
   end
   
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     
     flash[:success] = "Task は正常に削除されました"
@@ -53,9 +51,13 @@ class TasksController < ApplicationController
   
   private
   
+  def set_task
+    @task = Task.find(params[:id])
+  end
+  
   #Strong Parameter
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
   end
   
  
